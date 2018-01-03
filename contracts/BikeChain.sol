68df3f038cefbd1be4e48a92cdc9a50d19925d33
Samuel-Fiddis <sam.fiddis@gmail.com>
2018-01-03 18:39:35 +0000

    Updated BikeChain.sol to include current ERC-721 specification functions.

--- contracts/BikeChain.sol
+++ contracts/BikeChain.sol
@@ -22,6 +22,10 @@ contract Contacts{
 
 contract BikeChain is Contacts{
 
+    string contractName;
+    string contractSymbol;
+    uint256 supplyNum;
+
     struct Bike{
         address owner;
         string make;
@@ -32,6 +36,7 @@ contract BikeChain is Contacts{
         string features;
         string details;
         bool stolen;
+        string infoUrl;
     }
 
     // Switch to turn company admin off or on
@@ -43,6 +48,8 @@ contract BikeChain is Contacts{
     address[] public company;
     mapping(string => Bike) register; // Maps frame number to bike
     mapping(address => string[]) ownerList; // Maps address to frame numbers
+    // mapping(address => uint256) balance; Maps address to number of bikes owned
+    mapping(string => address) approvedAddress; // Maps frame number to address which is approved to take ownership
 
     modifier onlyAdmin(uint _cID){
         if(adminSwitch) require(msg.sender == company[_cID]);
@@ -87,7 +94,12 @@ contract BikeChain is Contacts{
 
     modifier bikeOwner(string _frameNumber){
         require(register[_frameNumber].owner == msg.sender);
-            _;
+        _;
+    }
+
+    modifier hasApproval(string _frameNumber){
+        require(approvedAddress[_frameNumber] == msg.sender);
+        _;
     }
 
     modifier costs(uint _price) {
@@ -95,6 +107,7 @@ contract BikeChain is Contacts{
             _;
     }
 
+    event Approval(address owner, address to, string frameNumber);
     event BikeCreated(string frameNumber);
     event BikeRemoved(address company, address owner, string frameNumber);
     event BikeTransfered(address from, address to, string frameNumber);
@@ -111,7 +124,84 @@ contract BikeChain is Contacts{
         company[0] = msg.sender;
         registrationPrice = 10000000000000000;
         adminSwitch = false;
+        contractName = "Bike-Token";
+        contractSymbol = "🚲";
+        supplyNum = 0;
     }
+
+    // Begin ERC-721 Standard Functions
+
+    function name() constant public returns (string){
+      return contractName;
+    }
+
+    function symbol() constant public returns (string){
+      return contractSymbol;
+    }
+
+    function totalSupply() constant public returns (uint256){
+      return supplyNum;
+    }
+
+    function balanceOf(address _owner) constant public returns (uint256){
+      return ownerList[_owner].length;
+    }
+
+    function ownerOf(string _frameNumber) constant public returns (address){
+      require(register[_frameNumber].owner != 0);
+      return register[_frameNumber].owner;
+    }
+
+    function approve(address _to, string _frameNumber) bikeOwner(_frameNumber) public{
+      approvedAddress[_frameNumber] = _to;
+      Approval(msg.sender, _to, _frameNumber);
+    }
+
+    function takeOwnership(string _frameNumber) hasApproval(_frameNumber) public{
+      address oldOwner = register[_frameNumber].owner;
+      ownerList[msg.sender].push(_frameNumber);
+      register[_frameNumber].owner = msg.sender;
+      string[] storage fn = ownerList[oldOwner];
+      uint index = getFrameIndex(oldOwner, _frameNumber);
+      for (uint i = index; i < fn.length-1; i++){
+        fn[i] = fn[i+1];
+      }
+      delete fn[fn.length-1];
+      fn.length--;
+
+      BikeTransfered(oldOwner, msg.sender, _frameNumber);
+    }
+
+    function transfer(address _to, string _frameNumber) bikeOwner(_frameNumber) public{
+      require(_to != 0);
+      ownerList[_to].push(_frameNumber);
+      register[_frameNumber].owner = _to;
+      string[] storage fn = ownerList[msg.sender];
+      uint index = getFrameIndex(msg.sender, _frameNumber);
+      for (uint i = index; i < fn.length-1; i++){
+        fn[i] = fn[i+1];
+      }
+      delete fn[fn.length-1];
+      fn.length--;
+
+      BikeTransfered(msg.sender, _to, _frameNumber);
+    }
+
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) constant public returns (string frameNumber){
+      require(_index < balanceOf(_owner));
+      require(_index >= 0);
+      frameNumber = ownerList[_owner][_index];
+    }
+
+    function tokenMetadata(string _frameNumber) constant public returns (string){
+      return register[_frameNumber].infoUrl;
+    }
+
+    function updateTokenMetadata(string _frameNumber, string _infoUrl) bikeOwner(_frameNumber) constant public{
+      register[_frameNumber].infoUrl = _infoUrl;
+    }
+
+    // End ERC-721 Standard Functions
 
     function () payable public{}
 
@@ -121,7 +211,7 @@ contract BikeChain is Contacts{
         EtherWithdrawn(msg.sender, _to, _value);
     }
 
-    function addBike(string _frameNumber, string _make, string _model, uint16 _year, uint8 _size, string _colour, string _features, uint _cID)
+    function addBike(string _frameNumber, string _make, string _model, uint16 _year, uint8 _size, string _colour, string _features, string _infoUrl, uint _cID)
     payable onlyAdmin(_cID) costs(registrationPrice) public{
         require(register[_frameNumber].owner == 0); // Check bike isn't owned
 
@@ -134,6 +224,8 @@ contract BikeChain is Contacts{
         b.colour = _colour;
         b.features = _features;
         b.stolen = false;
+        b.infoUrl = _infoUrl;
+        supplyNum++;
         BikeCreated(_frameNumber);
 
         ownerList[msg.sender].push(_frameNumber);
@@ -158,6 +250,7 @@ contract BikeChain is Contacts{
     }
 
     function transferOwner(string _frameNumber, address _newOwner, uint _cID) onlyAdminOrOwner(_cID, _frameNumber) public{
+        require(_newOwner != 0);
         ownerList[_newOwner].push(_frameNumber);
         register[_frameNumber].owner = _newOwner;
         string[] storage fn = ownerList[msg.sender];
@@ -181,6 +274,7 @@ contract BikeChain is Contacts{
         }
         delete fn[fn.length-1];
         fn.length--;
+        supplyNum--;
 
         BikeRemoved(msg.sender, owner, _frameNumber);
     }
@@ -213,7 +307,7 @@ contract BikeChain is Contacts{
 
     // Get functions
     function getBike(string _frameNumber) constant public
-    returns(address owner, string make, string model, uint16 year, uint8 size, string colour, string features, string details, bool stolen){
+    returns(address owner, string make, string model, uint16 year, uint8 size, string colour, string features, string details, bool stolen, string infoUrl){
 
     Bike storage b = register[_frameNumber];
     owner = b.owner;
@@ -225,6 +319,7 @@ contract BikeChain is Contacts{
     features = b.features;
     details = b.details;
     stolen = b.stolen;
+    infoUrl = b.infoUrl;
     }
 
     function getFrameIndex(address _owner, string _frameNumber) constant public returns(uint i){
@@ -257,8 +352,8 @@ contract BikeChain is Contacts{
     }
 
     function getContactDetails(address _owner) constant public
-    returns(string name, string email, string phone){
-        name = contactList[_owner].name;
+    returns(string contactName, string email, string phone){
+        contactName = contactList[_owner].name;
         email = contactList[_owner].email;
         phone = contactList[_owner].phone;
     }
